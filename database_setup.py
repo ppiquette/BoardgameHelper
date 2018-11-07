7fb5e0243234bdd22eef0bb77c7a679358065956
ppiquette <ppiquette@gmail.com>
2018-11-06 21:35:11 -0500

    end of day

--- database_setup.py
+++ database_setup.py
@@ -7,21 +7,21 @@ from sqlalchemy import create_engine
 Base = declarative_base()
 
 
-class Restaurant(Base):
-    __tablename__ = 'restaurant'
-    name = Column(String(80), nullable = False)
-    id = Column(Integer, primary_key = True)
+class User(Base):
+    __tablename__ = 'user'
+    name = Column(String(80), nullable=False)
+    id = Column(Integer, primary_key=True)
 
 
-class MenuItem(Base):
-    __tablename__ = 'menu_item'
-    name = Column(String(80), nullable = False)
-    id = Column(Integer, primary_key = True)
-    course = Column(String(250))
+class HeroRealmEvent(Base):
+    __tablename__ = 'hero_realm_event'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(150))
     description = Column(String(250))
-    price = Column(String(8))
-    restaurant_id = Column(Integer, ForeignKey(Restaurant.id))
-    restaurant = relationship(Restaurant)
+    user1_id = Column(Integer, ForeignKey(User.id))
+    user1 = relationship(User)
+    user2_id = Column(Integer, ForeignKey(User.id))
+    user2 = relationship(User)
 
     def __repr__(self):
         return "Name: %s" % self.name
@@ -30,12 +30,11 @@ class MenuItem(Base):
     def serialize(self):
         return {
             'name' : self.name,
-            'description': self.description,
-            'price' : self.price,
             'id' : self.id
         }
 
-engine = create_engine('sqlite:///restaurant.db')
+
+engine = create_engine('sqlite:///boardgamehelper.db')
 Base.metadata.create_all(engine)
 
 
